6ba02827a88c6662c45af7a832d4df81e921b1e8
johan <johanwirasugianto@gmail.com>
2023-06-28 09:45:59 +0800

    Add DevTools Dependency

--- src/main/java/com/demo/springboot3/rest/FunRestController.java
+++ src/main/java/com/demo/springboot3/rest/FunRestController.java
@@ -1,7 +1,16 @@
 package com.demo.springboot3.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FunRestController {
 
+    // expose "/" that return "Hello World!"
 
+    @GetMapping("/")
+    public String sayhello(){
+        return "Hello World!";
+    }
 
 }
